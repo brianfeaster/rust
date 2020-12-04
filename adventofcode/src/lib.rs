45b748775564afc043747b3ebb6b2885487d6ab6
Brian Feaster <bfeaster@uplift.com>
2020-12-04 01:36:32 -0800

    AdventOfCode day4

--- adventofcode/src/lib.rs
+++ adventofcode/src/lib.rs
@@ -62,8 +62,7 @@ fn day1 () {
 // Day 2
 // 
 fn validate_passwords (filename: &str) -> usize {
-    Regex::new(r"(\d+)-(\d+) (.): (.*)")
-    .unwrap()
+    Regex::new(r"(\d+)-(\d+) (.): (.*)").unwrap()
     .captures_iter(&::std::fs::read_to_string(filename).unwrap())
     .map( |cap| {
         let from = cap[1].parse::<usize>().unwrap();
@@ -104,36 +103,88 @@ fn day2 () {
 ////////////////////////////////////////////////////////////////////////////////
 // Day 3
 
-fn walk ( filename: &str, skiptable: &[(usize, usize)]) -> usize {
-    ::std::fs::read_to_string(filename).unwrap().lines()
-    .fold(
-        (0,1,0,0),
-        |(idx, rowskip, col, result), row| {
-        if rowskip == 1 {
-            (idx+1,
-             skiptable[idx % skiptable.len()].1,
-             col + skiptable[idx % skiptable.len()].0,
-             result + (0 < row.len() &&
-                      '#' == row.chars().nth(col % row.len()).unwrap() ) as usize )
-        } else {
-           (idx, rowskip-1, col, result)
-        }
-    }
-    ).3
+fn walks (filename: &str, dirs: &[(usize, usize)]) -> usize {
+    dirs.iter()
+    .map( |(dx, dy)|
+        ::std::fs::read_to_string(filename).unwrap().lines()
+        .step_by(*dy)
+        .enumerate()
+        .filter(|(i, line)| Some('#') == line.chars().nth(i * *dx % line.len()))
+        .count())
+    .product::<usize>()
 }
-
 fn day3 () {
     ::std::println!("== {}:{} ::{}::day3() ====", std::file!(), core::line!(), core::module_path!());
-    println!("First stage result {}", walk("data/input3.txt", &[(3,1)]));
-    println!("Second stage result {:?}",
-        [(1,1),(3,1),(5,1),(7,1),(1,2)]
-        .iter()
-        .map( |p| walk("data/input3.txt", &[*p]) )
-        .product::<usize>()
-    );
+    vec![ vec![(3, 1)], vec![(1,1),(3,1),(5,1),(7,1),(1,2)] ]
+    .iter()
+    .for_each( |dirs| println!("Result = {} for {:?}", walks("data/input3.txt", dirs), dirs) );
 }
 
 // Day 3
+////////////////////////////////////////////////////////////////////////////////
+// Day 4
+
+type Hm4 = HashMap<String, String>;
+
+fn scan_passport (filename: &str) -> Vec<Hm4> {
+    ::std::fs::read_to_string(filename).unwrap().lines()
+    .fold(
+        vec![HashMap::new()], // Initial vector contains empty hash table
+        |mut v, line| {
+            line.split(' ') // ["key:val", ...] or [""] when the line is empty
+            .for_each( |kvstr| { // "key:val" or ""
+                if kvstr == "" {
+                    v.insert(0, HashMap::new()) // Start a new hash table
+                } else {
+                    // Add k/v to hashtable at head of vector
+                    let kv = kvstr.split(':').collect::<Vec<&str>>();
+                    v[0].insert(kv[0].to_string(), kv[1].to_string());
+                }
+            });
+            v // Return the vector (is this copied each itme?) for next fold iteration
+        }
+    ) // fold
+}
+
+fn validate_passport1 (h: &Hm4) -> bool {
+    h.len() == 8 || (h.len()==7 && None == h.get("cid"))
+}
+
+fn validate_passport2 (h: &Hm4) -> bool {
+    let s = &"0".to_string(); // Bogus string
+    validate_passport1(h)
+    && {
+        let byr = h.get("byr").unwrap_or(s).parse::<i32>().unwrap();
+        1920 <= byr && byr <= 2002
+    } && {
+        let iyr = h.get("iyr").unwrap_or(s).parse::<i32>().unwrap();
+        2010 <= iyr && iyr <= 2020
+    } && {
+        let eyr = h.get("eyr").unwrap_or(s).parse::<i32>().unwrap();
+        2020 <= eyr && eyr <= 2030
+    } && {
+        let cap = Regex::new(r"(\d+)(.*)").unwrap().captures(h.get("hgt").unwrap_or(s)).unwrap();
+        let h = cap[1].parse::<i32>().unwrap();
+        let u = &cap[2];
+        (u=="cm" && (150 <= h && h <= 193)) ||
+        (u=="in" && (59  <= h && h <= 76))
+    } && {
+        1 == Regex::new(r"^#[0-9a-f]{6}$").unwrap().captures_iter(h.get("hcl").unwrap_or(s)).count()
+    } && {
+        1 == Regex::new(r"^(amb|blu|brn|gry|grn|hzl|oth)$").unwrap().captures_iter(h.get("ecl").unwrap_or(s)).count()
+    } && {
+        1 == Regex::new(r"^\d{9}$").unwrap().captures_iter(h.get("pid").unwrap_or(s)).count()
+    }
+}
+
+fn day4 () {
+    ::std::println!("== {}:{} ::{}::day4() ====", std::file!(), core::line!(), core::module_path!());
+    println!("Valid passports v1 {:?}",
+        scan_passport("data/input4.txt").iter().filter( |h| validate_passport1(*h) ).count());
+    println!("Valid passports v2 {:?}",
+        scan_passport("data/input4.txt").iter().filter( |h| validate_passport2(*h) ).count());
+}
+// Day 4
 ////////////////////////////////////////////////////////////////////////////////
 // Main
 
@@ -152,10 +203,13 @@ pub fn main() {
     day2();
     // Valid passwords: 483
     // Valid passwords 2nd approach: 482
-    }
     day3();
     // First stage result 286
     // Second stage result 3638606400
+    }
+    day4();
+    // Valid passports v1 242
+    // Valid passports v2 186
 }
 
 // Main
